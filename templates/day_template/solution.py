c334ccaba2093b710a86cfb2e8135e13ffe825bc
Ricardo Vitorino <hello@ricardovitorino.com>
2024-12-01 18:37:04 +0000

    feat: add daily solution and test templates
    Add templates/day_template/:
      * solution.py
      * test_solution.py
      * input.txt
      * test_input.txt

---
+++ templates/day_template/solution.py
@@ -0,0 +1,16 @@
+def part1(data):
+    # Solve part 1
+    pass
+
+
+def part2(data):
+    # Solve part 2
+    pass
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        data = f.read().splitlines()
+
+    print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")
